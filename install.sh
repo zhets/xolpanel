644336b51b752a0e8dde287906a3cb2c52cc04a2
XDXL Project <142297155+zhets@users.noreply.github.com>
2024-02-04 12:27:11 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
 rm -r /etc/xdxl
+mkdir -p /etc/xdxl
+domain=$(cat /etc/xray/domain)
+SYSTEM=systemctl
+xd=xolpanel
+bot=/etc/xdxl
 apt update && apt upgrade -y
 apt install python3 python3-pip -y
 apt install sqlite3 -y
-mkdir -p /etc/xdxl
 cd /etc/xdxl
-git clone https://github.com/zhets/xolpanel.git
-cd xolpanel
-mv xolpanel.service /etc/systemd/system/
+git clone https://github.com/zhets/${xd}.git
+cd $xd
+mv ${xd}.service /etc/systemd/system/
 pip3 install -r requirements.txt
 pip install pillow
 pip install speedtest-cli
-domain=$(cat /etc/xray/domain)
 clear
 echo -e "  INSTALL BOT CREATE SSH VIA TELEGRAM"
 echo -e " Masukan Info Bot Kamu ! "
 read -e -p "[*] Input Your Token Bot : " token
 read -e -p "[*] Input Your Id Telegram : " admin
-echo -e ADMIN='"'$admin'"' >> /etc/xdxl/xolpanel/var.txt
-echo -e BOT_TOKEN='"'$token'"' >> /etc/xdxl/xolpanel/var.txt
-echo -e DOMAIN='"'$domain'"' >> /etc/xdxl/xolpanel/var.txt
+echo -e ADMIN='"'$admin'"' >> $bot/$xd/var.txt
+echo -e BOT_TOKEN='"'$token'"' >> $bot/$xd/var.txt
+echo -e DOMAIN='"'$domain'"' >> $bot/$xd/var.txt
 
-systemctl daemon-reload
-systemctl start xolpanel
-systemctl enable xolpanel
-systemctl restart xolpanel
+$SYSTEM daemon-reload
+$SYSTEM start $xd
+$SYSTEM enable $xd
+$SYSTEM restart $xd
 clear
 echo -e ""
 echo " Installations complete, type /menu on your bot "
